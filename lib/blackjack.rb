d5d70be5982e237ea242e5966abd9f10c87f9bae
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-24 15:05:20 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,61 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  return rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(total)
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  num1 = deal_card
+  num2 = deal_card
+  sum = num1 + num2
+  display_card_total(sum)
+  sum
 end
 
-def hit?
-  # code hit? here
+
+def hit?(sum)
+  prompt_user
+  newinput = get_user_input
+  
+  until newinput == "h" || newinput == "s"
+    invalid_command
+    prompt_user
+    newinput = get_user_input
+  end
+  
+  loop newinput == "s" do
+    prompt_user
+    break if newinput == "h"
+  end
+  
+  if newinput == "h"
+    newtotal = sum + deal_card
+ 
+  end
+  newtotal
 end
+
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,21 @@
 require_relative "blackjack.rb"
 
+welcome 
+
+initial_round 
+
+prompt_user
+
+until newtotal > 21
+  hit?
+  if newinput == "s"
+    hit?
+  end
+  display_card_total
+end 
+
+end_game 
+end 
+
+
 runner
